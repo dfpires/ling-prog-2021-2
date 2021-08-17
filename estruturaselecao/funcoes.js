3540a1744c92f1fede46eec44d0aa053066850c8
Daniel Facciolo Pires <dfpires@gmail.com>
2021-08-17 10:51:34 -0300

    17-08

--- estruturaselecao/funcoes.js
+++ estruturaselecao/funcoes.js
@@ -16,3 +16,36 @@ function exe1(){
         document.getElementById("resultado").innerHTML = `A média é ${media} está reprovado`
     }
 }
+
+function exe2(){
+    // entrada de dados
+    let nota1 = Number(document.getElementById("nota1").value)
+    let nota2 = Number(document.getElementById("nota2").value)
+    // calcula a média
+    let media = (nota1 + nota2) / 2
+    // operadores lógicos
+    // E -> &&
+    let resultado 
+    if ((media >=0) && (media < 3)){
+
+        resultado = `Aluno aprovado com média ${media}`
+
+    }
+    else if ((media >= 3) && (media < 7)){
+
+        resultado = `Aluno em exame com média ${media}`
+
+    }
+    else if ((media >= 7 ) && (media <= 10)){
+
+        resultado = `Aluno reprovado com média ${media}`
+
+    }
+    else {
+
+        resultado = `Notas informadas são inválidas`
+
+    }
+    // mostra resultado
+    document.getElementById("resultado").innerHTML = resultado
+}
