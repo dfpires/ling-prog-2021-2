a99678fbcd387c6e66f0a615f4b8f974febe5871
Daniel Pires <dfpires@gmail.com>
2021-09-28 10:25:30 -0300

    28-09

---
+++ vetores/funcoes.js
@@ -0,0 +1,25 @@
+function exe1(){
+    // declarar os vetores
+    let vetor = []
+    let impares = []
+    let pares = []
+    // entrada de dados
+    for(let i=0;i<6;i++){
+        vetor[i] = Number(prompt(`Informe o número ${i+1}`))
+    }
+    // alimentar os vetores pares e impares
+    // para não precisarmos controle o índice dos vetores destinos, usamos push()
+    // push() já sabe a primeira posição vazia do vetor
+    for(let i=0;i<6;i++){
+        if (vetor[i] % 2 == 0) {
+            // elemento par
+            pares.push(vetor[i])            
+        }
+        else {
+            // elemento ímpar
+            impares.push(vetor[i])       
+        }
+    }
+    console.log(`Elementos pares ${pares}`)
+    console.log(`Elementos ímpares ${impares}`)
+}
