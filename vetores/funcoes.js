ac57de7d3ec79a2b9ff6ad759166dfc0423a6af7
Daniel Pires <dfpires@gmail.com>
2021-09-28 10:49:13 -0300

    28-09

--- vetores/funcoes.js
+++ vetores/funcoes.js
@@ -25,5 +25,36 @@ function exe1(){
 }
 
 function exe2(){
-    
+     // declarar os vetores
+     let vetor = []
+     let m2 = []
+     let m3 = []
+     let m23 = []
+    // entrada de dados
+    for(let i=0;i<7;i++){
+        vetor[i] = Number(prompt(`Informe o número ${i+1}`))
+    }
+     // alimentar os vetores m2, m3 e m23
+    // para não precisarmos controle o índice dos vetores destinos, usamos push()
+    // push() já sabe a primeira posição vazia do vetor
+    for(let i=0;i<7;i++){
+        if (vetor[i] % 2 == 0) {
+            // elemento é múltiplo de 2
+            m2.push(vetor[i])            
+        }
+        if (vetor[i] % 3 == 0) {
+            // elemento ímpar
+            m3.push(vetor[i])       
+        }
+        if ((vetor[i] % 2 == 0) && (vetor[i] % 3 == 0)) {
+            // elemento ímpar
+            m23.push(vetor[i])       
+        }
+    }
+    console.log(`Elementos múltiplos de 2: ${m2} e qtde ${m2.length}`)
+    console.log(`Elementos múltiplo de 3: ${m3} e qtde ${m3.length}`)
+    console.log(`Elementos múltiplo de 2 e 3: ${m23} e qtde ${m23.length}`)
+    alert(`Elementos múltiplos de 2: ${m2} e qtde ${m2.length}`)
+    alert(`Elementos múltiplo de 3: ${m3} e qtde ${m3.length}`)
+    alert(`Elementos múltiplo de 2 e 3: ${m23} e qtde ${m23.length}`)
 }
